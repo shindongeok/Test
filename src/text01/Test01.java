bd08a68a93d854f374a15c2f082a558dbeed8fc0
shindongeok <ehrflqakstp@gmail.com>
2024-11-06 12:35:06 +0900

    test02

---
+++ src/text01/Test01.java
@@ -0,0 +1,25 @@
+package text01;
+
+class Person{
+
+}
+class Student extends Person {
+
+}
+class Entertainer extends Person {
+
+}
+class Singer extends Person {
+
+}
+public class Test01 {
+    static void pr(Person p){
+
+    }
+    public static void main(String[] args){
+        pr(new Student());
+        pr(new Entertainer());
+        pr(new Singer());
+
+    }
+}
