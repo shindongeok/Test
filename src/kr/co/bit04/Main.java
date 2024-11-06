522b58cd56aecd74fd27d8b1b07bbd4700bfe7dd
shindongeok <ehrflqakstp@gmail.com>
2024-11-06 14:19:04 +0900

    abstract class

--- src/kr/co/bit04/Main.java
+++ src/kr/co/bit04/Main.java
@@ -1,7 +1,29 @@
 package kr.co.bit04;
 
+abstract class Ca{
+    abstract int add(int a, int b);
+    abstract double avg(int a[]);
+}
+class Cal extends Ca{
+    @Override
+    int add(int a, int b){
+        return a + b;
+    }
+
+    @Override
+    double avg(int a[]){
+        int sum = 0;
+        for(int i=0; i<a.length; i++){
+            sum+=a[i];
+        }
+        return (double) sum/a.length;
+    }
+}
 public class Main {
     public static void main(String[] args){
-//  마스터에서 메인으로 브랜치 변경
+
+        Cal c =new Cal();
+        System.out.println(c.add(3,7)); //두수합
+        System.out.println(c.avg(new int[] {1,2,3}));   //1,2,3 평균
     }
 }
