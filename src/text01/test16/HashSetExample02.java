730db87a945a88463923afdd82824f01ce25b0e7
shindongeok <ehrflqakstp@gmail.com>
2024-11-11 20:45:00 +0900

    Collection HashSetExample03.java

--- src/text01/test16/HashSetExample02.java
+++ src/text01/test16/HashSetExample02.java
@@ -1,5 +1,8 @@
 package text01.test16;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class Member{
     String name;
     int age;
@@ -12,10 +15,26 @@ class Member{
     public int hashCode(){
         return name.hashCode() + age;
     }
-
+    @Override
+    public boolean equals(Object obj){
+        if(obj instanceof  Member target){
+            return target.name.equals(name) && (target.age == age);
+        }
+        else {
+            return false;
+        }
+    }
 }
 public class HashSetExample02 {
     public static void main(String[] args){
+        //HashSet 컬렉션 생성
+        Set<Member> set = new HashSet<Member>();
 
+        //Member  객체 저장
+        set.add(new Member("홍길동", 30));
+        set.add(new Member("홍길동", 30));
+
+        //저장된 객체수 출력.
+        System.out.println("저장된 객체수는 : " + set.size());
     }
 }
---
+++ src/text01/test17/HashSetExample03.java
@@ -0,0 +1,39 @@
+package text01.test17;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class HashSetExample03 {
+    public static void main(String[] args){
+        //HashSet 컬렉션 생성
+        Set<String> str1 =new HashSet<String>();
+
+        str1.add("Java");
+        str1.add("JDBC");
+        str1.add("JSP");
+        str1.add("Spring");
+
+        //객체를 하나씩 가져와서 처리
+        Iterator<String> it = str1.iterator();
+        while (it.hasNext()){
+            String sum = it.next();
+            System.out.println(sum);
+            if (sum.equals("JSP")){
+                //가져온 객체를 컬렉션에서 제거
+                it.remove();
+            }
+        }
+        System.out.println();
+
+        //객체 제거
+        str1.remove("JDBC");
+        str1.remove("Spring");
+
+        //객체를 하나씩 가져와서 처리
+        for(String sum : str1){
+            System.out.println(sum);
+        }
+
+    }
+}
